07d6036d905ee99281d28ff5f851b2856a307b17
Jonas <j_eckhardt@outlook.de>
2024-05-03 13:25:55 +0200

    Test2

--- Hoopstress_Calc.py
+++ Hoopstress_Calc.py
@@ -3,35 +3,35 @@ import numpy as np
 
 # Berechne sigma_z
 
-s_z = 0
+s_z = 0 #
 
 # Berechne sigma_phi_i
 
-def term(n,r_2):
+def term(n,r_2, r_1):
     """nützlicher Term"""
-    return (1/n) * (r_2**n - r_i**n)
+    return (1/n) * (r_2**n - r_1**n)
 
-r_i = 430 # [mm]
-r_a = 646 # [mm]
+r_i = 430 # [mm] innerer radius
+r_a = 646 # [mm] äußerer radius
 
 r = np.linspace(r_i,r_a,216)
 
-nu = 0.3 # [-]
-p = 1 - nu # [-]
+nu = 0.3 # [-] possion's ratio
+p = 1 - nu # [-] const.
 
-j = 114.2 # [A/mm^2]
-b_za = -2.5 # [T]
-b_zi = 15 # [T]
-b_0 = 49.84 # [T]
+j = 114.2 # [A/mm^2] Stromdichte
+b_za = -2.5 # [T] magnetische Flussdichte außen
+b_zi = 15 # [T] magnetische Flussdichte innen
+b_0 = 49.84 # [T] absolutes Glied der Geradengleichung für B(r)
 
 # j = 100
 # b_za = -1
 # b_zi = 10
 # b_0 = 31.9
 
-a = j * (b_za - b_zi)/(r_a - r_i) * term(2,r_a) + j * b_0 * term(1,r_a)
+a = j * (b_za - b_zi)/(r_a - r_i) * term(2,r_a, r_i) + j * b_0 * term(1,r_a, r_i)
 
-b = j * (b_za - b_zi)/(r_a - r_i) * term(4,r_a) + j * b_0 * term(3,r_a)
+b = j * (b_za - b_zi)/(r_a - r_i) * term(4,r_a, r_i) + j * b_0 * term(3,r_a, r_i)
 
 s_phi_i = ( (2/(1-(r_i**2 / r_a**2))) * ((p/2) * a + 1/r_a**2 * (1 - p/2) * b) ) / 1000 # [MPa = N/mm^2]
 
@@ -39,19 +39,24 @@ print(s_phi_i)
 
 # Berechne sigma_r(r)
 
-s_r = 1/2 * s_phi_i * (1 - r_i**2/r**2) - 1/2 * p * ((j * (b_za - b_zi)/(r_a - r_i) * term(2,r) + j * b_0 * term(1,r)) / 1000) - 1/r**2 * (((1 - p/2) * j * (b_za - b_zi)/(r_a - r_i) * term(4,r) + j * b_0 * term(3,r)) / 1000)
+s_r = 1/2 * s_phi_i * (1 - r_i**2/r**2) - 1/2 * p * ((j * (b_za - b_zi)/(r_a - r_i) * term(2,r, r_i) + j * b_0 * term(1,r, r_i)) / 1000) - (1/r**2 * (1 - p/2) * (j * (b_za - b_zi)/(r_a - r_i) * term(4,r, r_i) + j * b_0 * term(3,r, r_i))) / 1000
 
 print(s_r)
 
-s_phi = s_phi_i - p * ((j * (b_za - b_zi)/(r_a - r_i) * term(2,r) + j * b_0 * term(1,r)) / 1000) - s_r
+s_phi = s_phi_i - p * ((j * (b_za - b_zi)/(r_a - r_i) * term(2,r, r_i   ) + j * b_0 * term(1,r, r_i)) / 1000) - s_r
+
+f = -75 + 13 * (r - 420)**(1/3)
+for i,element in f
 
 plt.plot(r, s_phi, label='hoop stresses over radius', linewidth=2)
+plt.plot(r, (s_phi+f), label='hoop stresses over radius', linewidth=2)
 plt.grid()
 plt.xlabel('r, [mm]')
 plt.ylabel(r'Sigma_phi, [MPa]')
 plt.title('Hoop Stresses over radius')
 plt.legend
 plt.show()
+
 
 plt.plot(r, s_r, label='radial stresses over radius', linewidth=2)
 plt.grid()
